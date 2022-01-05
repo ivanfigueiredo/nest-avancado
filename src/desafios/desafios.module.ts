fd512c0b6483c49b267d8a0728131fa672c1859d
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 18:18:37 -0300

    chore: create service desafios

--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DesafiosController } from './desafios.controller';
+import { DesafiosService } from './desafios.service';
 
 @Module({
-  controllers: [DesafiosController]
+  controllers: [DesafiosController],
+  providers: [DesafiosService]
 })
 export class DesafiosModule {}
