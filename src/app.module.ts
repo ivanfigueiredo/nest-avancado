15c56be3f6070adc1fd3bc2985ac632b43e1e915
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 11:01:49 -0300

    feat: create module categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://127.0.0.1:27017/smartranking'
     ),
-    JogadoresModule
+    JogadoresModule,
+    CategoriasModule
   ],
   controllers: [],
   providers: [],
