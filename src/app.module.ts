fcfe5d0f2c363380bdd0c506a6588bb92bb794b5
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 07:41:13 -0300

    chore: add module of players to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/smartranking'
+    ),
+    JogadoresModule
+  ],
   controllers: [],
   providers: [],
 })
