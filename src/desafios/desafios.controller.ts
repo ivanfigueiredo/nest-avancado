2e3c13ce32d7cf0228f167cc4a95e9eae0b63ca1
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 20:37:02 -0300

    feat: create criarDesafio Controller and desafios.services to persist in MongoDB repository

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { DesafiosService } from './desafios.service';
+import { Body, Controller, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Desafio } from './interfaces/desafio.interface';
 
-@Controller('desafios')
-export class DesafiosController {}
+@Controller('api/v1/desafios')
+export class DesafiosController {
+
+    constructor(
+        private readonly desafioService: DesafiosService
+    ) {}
+
+    private readonly logger = new Logger(DesafiosController.name)
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarDesafio(
+        @Body() criarDesafioDto: CriarDesafioDto
+    ): Promise<Desafio> {
+        this.logger.log(`criarDesafios: ${JSON.stringify(criarDesafioDto)}`)
+        return await this.desafioService.criarDesafio(criarDesafioDto)
+    }
+
+}
--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -1,4 +1,60 @@
+import { BadRequestException, Logger } from '@nestjs/common';
+import { CategoriasService } from './../categorias/categorias.service';
+import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { Desafio } from './interfaces/desafio.interface';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JogadoresService } from 'src/jogadores/jogadores.service';
+import { DesafioStatus } from './interfaces/desafio-status.enum';
+
 
 @Injectable()
-export class DesafiosService {}
+export class DesafiosService {
+    constructor(
+        @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
+        @InjectModel('Partida') private readonly partidaModel: Model<Partida>,
+        private readonly jogadoresService: JogadoresService,
+        private readonly categoriasService: CategoriasService
+    ) {}
+
+    private readonly logger = new Logger(DesafiosService.name)
+
+    async criarDesafio(criarDesafioDto: CriarDesafioDto): Promise<Desafio> {
+      const jogadores = await this.jogadoresService.consultarTodosJogadores()
+
+      criarDesafioDto.jogadores.map(jogadorDto => {
+          const jogadorFilter = jogadores.filter( jogador => jogador.id == jogadorDto.id)
+
+          if (jogadorFilter.length == 0) {
+              throw new BadRequestException(`0 id ${jogadorDto.id} não é um jogador`)
+          }
+      })
+
+      const solicitanteEhJogadorDaPartida = criarDesafioDto.jogadores.filter(jogador => jogador.id == criarDesafioDto.solicitante)
+
+      this.logger.log(`solicitanteEhJogadorDaPartida: ${solicitanteEhJogadorDaPartida}`)
+
+      if (solicitanteEhJogadorDaPartida.length == 0) {
+          throw new BadRequestException(`0 solicitante deve ser um jogador da partida!`)
+      }
+
+      const categoriaDoJogador = await this.categoriasService.consultarCategoriaDoJogador(criarDesafioDto.solicitante)
+
+      if (!categoriaDoJogador) {
+          throw new BadRequestException('O solicitante precisa estar registrado em uma categoria')
+      }
+
+      const desafioCriado = new this.desafioModel(criarDesafioDto)
+      desafioCriado.categoria = categoriaDoJogador.categoria
+      desafioCriado.dataHoraSolicitacao = new Date()
+      this.logger.log(`desafioCriado.dataHoraSolicitacao: ${desafioCriado.dataHoraSolicitacao}`)
+
+      desafioCriado.status = DesafioStatus.PENDENTE
+      this.logger.log(`desafioCriado: ${JSON.stringify(desafioCriado)}`)
+      return await desafioCriado.save()
+    }
+
+    
+
+}
