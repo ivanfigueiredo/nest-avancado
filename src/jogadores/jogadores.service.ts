4cf6a4db6f8dcd2da63897687f1e63036e8eec4e
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 07:39:51 -0300

    feat: ensure JogadoresServices to persist data

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,52 @@
+import { CriarJogadorDto } from './dtos/criar-jogador.dto'
+import { Injectable, Logger, NotFoundException } from '@nestjs/common'
+import { Jogador } from './interfaces/jogador.interface'
+import {  InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+
+
+@Injectable()
+export class JogadoresService {
+  private jogadores: Jogador[] = [];
+
+  constructor(
+    @InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>
+  ) {}
+
+  private readonly logger  = new Logger(JogadoresService.name)
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    const { email } = criarJogadorDto
+    const jogadorEncontrado = await this.jogadorModel.findOne({ email }).exec()
+    if (jogadorEncontrado) {
+        await this.atualizar(criarJogadorDto)
+    }
+    await this.criar(criarJogadorDto)
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return await this.jogadorModel.find().exec()
+  }
+
+  async consultarJogadoresPeloEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = await this.jogadorModel.findOne({ email }).exec()
+    if (!jogadorEncontrado) {
+        throw new NotFoundException(`Jogador com e-mail ${email} não encontrado`)
+    }
+    return jogadorEncontrado
+  }
+
+  async deletarJogadorPeloEmail(email: string): Promise<any> {
+    return await this.jogadorModel.remove({ email }).exec()
+  }
+
+  private async criar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+    const jogadorCriado = new this.jogadorModel(criarJogadorDto)
+    return await jogadorCriado.save()
+  }
+
+  private async atualizar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+    return await this.jogadorModel.findOneAndUpdate({email: criarJogadorDto.email}, 
+      { $set: criarJogadorDto}).exec()
+  }
+}
