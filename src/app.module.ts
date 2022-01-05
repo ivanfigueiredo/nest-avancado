1c5bf3fdf3912c416ced3cd9d4685a6bf1ae97a4
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 18:15:32 -0300

    chore: create module desafios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { CategoriasModule } from './categorias/categorias.module';
       'mongodb://127.0.0.1:27017/smartranking'
     ),
     JogadoresModule,
-    CategoriasModule
+    CategoriasModule,
+    DesafiosModule
   ],
   controllers: [],
   providers: [],
