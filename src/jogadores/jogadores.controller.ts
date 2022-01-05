c6ab580da33ff62ed052d2dee80411cc3741947f
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 08:25:44 -0300

    feat: create custom pipes and usage in controller

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common'
 import { CriarJogadorDto } from './dtos/criar-jogador.dto'
-import { Jogador } from './interfaces/jogador.interface';
+import { Jogador } from './interfaces/jogador.interface'
 import { JogadoresService } from './jogadores.service'
+import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validacao-parametros.pipe'
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -15,7 +16,7 @@ export class JogadoresController {
     }
   @Get()
   async consultarJogadores(
-    @Query('email') email: string
+    @Query('email', JogadoresValidacaoParametrosPipe) email: string
   ): Promise<Jogador[] | Jogador> {
     if (email) {
         return await this.jogadoresService.consultarJogadoresPeloEmail(email)
@@ -25,12 +26,9 @@ export class JogadoresController {
 
   @Delete()
   async deletarJogador(
-    @Query('email') email: string
+    @Query('email', JogadoresValidacaoParametrosPipe) email: string
   ): Promise<void> {
     await this.jogadoresService.deletarJogadorPeloEmail(email)
   }
-}
-function UserPipes() {
-  throw new Error('Function not implemented.');
 }
 
