550e9d244d433ced220de1130467e48e7cc7173b
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 11:58:56 -0300

    feat: create CategoriaController and CategoriaServices to persist MongoDB

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+    constructor(
+        private readonly categoriaService: CategoriasService
+    ){}
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarCategoria(
+        @Body() criarCategoriaDto: CriarCategoriaDto): Promise<Categoria> {
+        return await this.categoriaService.criarCategoria(criarCategoriaDto)
+    }
+}
