e20d096d346aae3b159921b913135492541cfa4a
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 07:45:52 -0300

    feat: ensure that Jogadores Crud routes

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto'
+import { Jogador } from './interfaces/jogador.interface';
+import { JogadoresService } from './jogadores.service'
 
-@Controller('jogadores')
-export class JogadoresController {}
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}    
+    
+  @Post()
+  async criarAtualizarJogador(
+    @Body() criarJogador: CriarJogadorDto) {
+      await this.jogadoresService.criarAtualizarJogador(criarJogador)
+    }
+  @Get()
+  async consultarJogadores(
+    @Query('email') email: string
+  ): Promise<Jogador[] | Jogador> {
+    if (email) {
+        return await this.jogadoresService.consultarJogadoresPeloEmail(email)
+    }
+    return this.jogadoresService.consultarTodosJogadores()
+  }
+
+  @Delete()
+  async deletarJogador(
+    @Query('email') email: string
+  ): Promise<void> {
+    await this.jogadoresService.deletarJogadorPeloEmail(email)
+  }
+}
