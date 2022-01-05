7fc05a4cbe96884becc275d8c86e62768aa8eefe
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2022-01-05 20:34:27 -0300

    feat: add DesafioService in module.desafios

--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DesafiosController } from './desafios.controller';
 import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafios.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Desafio', schema: DesafioSchema}])],
   controllers: [DesafiosController],
-  providers: [DesafiosService]
+  providers: [DesafiosService],
+  exports: [DesafiosService]
 })
 export class DesafiosModule {}
